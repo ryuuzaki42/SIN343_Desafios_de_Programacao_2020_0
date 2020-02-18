abd37a55fc3b084f3539b9be386c64b451bc9b18
João D. Batista <joao42lbatista@gmail.com>
2020-02-18 11:20:05 -0300

    Update 0readmeLinks.r, helloWorld.zip, and 5 more files...

---
+++ aulas/aula01/0readmeLinks.r
@@ -0,0 +1,10 @@
+http://www.cplusplus.com/
+
+https://www.ime.usp.br/~pf/algoritmos/index.html
+
+https://www.urionlinejudge.com.br/judge/pt/login
+https://onlinejudge.org/
+https://www.spoj.com/
+https://www.hackerrank.com/dashboard
+
+http://maratona.ime.usp.br/
---
+++ aulas/aula01/pratica01.r
@@ -0,0 +1,5 @@
+
+## Hello World em C, C++ e Java
+## Compilar pelo terminarl e enviar código para Compilar
+
+## Resolver m1 e enviar
