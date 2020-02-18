b3eb3174eeb4880ee40ba2b5f0fdd8fccaaee0ab
João D. Batista <joao42lbatista@gmail.com>
2020-02-18 11:20:48 -0300

    Update pratica05.r, pratica06.r, and 2 more files...

---
+++ aulas/aula04/pratica05.r
@@ -0,0 +1,2 @@
+
+## Cada equipe apresenta uma questão da prova 1 - Maratona 1
---
+++ aulas/aula04/pratica06.r
@@ -0,0 +1,3 @@
+
+## Resolver "1256 do URI" e enviar para o PVANet.
+https://www.urionlinejudge.com.br/judge/pt/problems/view/1256
